f57b49c8e4997260ad3ec319eb1b12a601d6567c
chickinnick <chickinnick@gmail.com>
2016-09-18 15:09:36 +0300

    settigs activity, seekbar

--- app/src/main/java/com/chickinnick/earnie/home/SettingsActivity.java
+++ app/src/main/java/com/chickinnick/earnie/home/SettingsActivity.java
@@ -2,14 +2,19 @@ package com.chickinnick.earnie.home;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
+import com.chickinnick.earnie.EarineApp;
 import com.chickinnick.earnie.R;
 import com.chickinnick.earnie.databinding.ActivitySettingBinding;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
+    private ActivitySettingBinding activitySettingsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,46 @@ public class SettingsActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-        ActivitySettingBinding activitySettingsBinding = DataBindingUtil.setContentView(this, R.layout.activity_setting);
+        activitySettingsBinding = DataBindingUtil.setContentView(this, R.layout.activity_setting);
+
+        Typeface typeface = EarineApp.getRegularTypeface();
+        activitySettingsBinding.paymentMethods.setTypeface(typeface);
+        activitySettingsBinding.profile.setTypeface(typeface);
+        activitySettingsBinding.about.setTypeface(typeface);
+        activitySettingsBinding.adCategories.setTypeface(typeface);
+        activitySettingsBinding.adFreqTitle.setTypeface(typeface);
+
+        activitySettingsBinding.paymentMethods.setOnClickListener(this);
+        activitySettingsBinding.profile.setOnClickListener(this);
+
+
+        activitySettingsBinding.adFreqSeekbar.setProgress(1);
+        activitySettingsBinding.adFreqSeekbar.setOnSeekBarChangeListener(this);
+
+
+        activitySettingsBinding.low.setOnClickListener(indicatorClick);
+        activitySettingsBinding.norm.setOnClickListener(indicatorClick);
+        activitySettingsBinding.high.setOnClickListener(indicatorClick);
 
     }
+
+
+    View.OnClickListener indicatorClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.low:
+                    activitySettingsBinding.adFreqSeekbar.setProgress(0);
+                    break;
+                case R.id.norm:
+                    activitySettingsBinding.adFreqSeekbar.setProgress(1);
+                    break;
+                case R.id.high:
+                    activitySettingsBinding.adFreqSeekbar.setProgress(2);
+                    break;
+            }
+        }
+    };
 
 
     @Override
@@ -27,5 +69,47 @@ public class SettingsActivity extends AppCompatActivity {
         startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.profile:
+                break;
+            case R.id.payment_methods:
+                break;
+
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (progress) {
+            case 0:
+                activitySettingsBinding.low.setTextColor(getResources().getColor(R.color.toolbar_title_blue));
+                activitySettingsBinding.norm.setTextColor(getResources().getColor(R.color.text_default_dark));
+                activitySettingsBinding.high.setTextColor(getResources().getColor(R.color.text_default_dark));
+                break;
+            case 1:
+                activitySettingsBinding.low.setTextColor(getResources().getColor(R.color.text_default_dark));
+                activitySettingsBinding.norm.setTextColor(getResources().getColor(R.color.toolbar_title_blue));
+                activitySettingsBinding.high.setTextColor(getResources().getColor(R.color.text_default_dark));
+                break;
+            case 2:
+                activitySettingsBinding.low.setTextColor(getResources().getColor(R.color.text_default_dark));
+                activitySettingsBinding.norm.setTextColor(getResources().getColor(R.color.text_default_dark));
+                activitySettingsBinding.high.setTextColor(getResources().getColor(R.color.toolbar_title_blue));
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
